2f3dfecd9deee4c1722272e52a5608dc9be00913
zhi <zhichen0428@gmail.com>
2025-04-04 14:43:26 -0400

    add 2d cartesian test

--- Exec/unit_tests/diffusion_test/analysis/slice_grid.py
+++ Exec/unit_tests/diffusion_test/analysis/slice_grid.py
@@ -11,8 +11,9 @@ Give a temperature slice plot for 2d plot file
 fname = sys.argv[1]
 ds = CastroDataset(fname)
 
-slice_dirs = {"cylindrical":"theta",
-             "spherical":"phi"}
+slice_dirs = {"cartesian": "z",
+              "cylindrical":"theta",
+              "spherical":"phi"}
 slice_dir = slice_dirs[ds.geometry]
 
 slc = yt.SlicePlot(ds, slice_dir, "Temp")
